427b36b01acae6981a83580dfe3df3f1dcf5218d
guelad <niassegueladio@gmail.com>
2021-11-07 13:15:27 +0100

    import and clean data

---
+++ data/Code.py
@@ -0,0 +1,18 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ipywidgets import interact  # widget manipulation
+from download import download  # download data / avoid redownloading
+pd.options.display.max_rows = 8
+
+#%%
+#Reading the file as a `pandas` dataframe:
+df = pd.read_csv('gares-peage-2019.csv', sep=";", header = None)
+#%%
+df1 =df.dropna()
+
+#%%
+df1
+
