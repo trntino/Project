178d97b9fb10ab6fe2c717abe968f573aac4ab5e
Quentin Fontana <quentin1220fontana@hotmail.fr>
2021-11-14 18:30:25 +0100

    data

--- data/Code.py
+++ data/Code.py
@@ -1,25 +1,66 @@
 #%%
-import numpy as np
-import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+#import alphashape
+#from descartes import PolygonPatch
+import folium
+import geopandas as gpd
+#from geopy.geocoders import Nominatim
+#from ipywidgets import interact, fixed, widgets
 import matplotlib.pyplot as plt
-import seaborn as sns
-from ipywidgets import interact  # widget manipulation
-from download import download  # download data / avoid redownloading
+import networkx as nx
+import numpy as np
+import osmnx as ox
+import pandas as pd
+from shapely import geometry
+#from pyroutelib3 import Router
+from pyproj import Proj, transform
+#import seaborn as sns
+#from ipywidgets import interact  # widget manipulation
+#from download import download  # download data / avoid redownloading
 pd.options.display.max_rows = 8
 
 #%%
 #Reading the file as a `pandas` dataframe:
-df = pd.read_csv('gares-peage-2019.csv', sep=";", header = None)
-#%%
-df1 =df.dropna()
+df = pd.read_csv('gares-peage-2019.csv', sep=";")
+df
 
 #%%
-#extraction l'entreprise ASF
-df11 =df1[df1[16]=='ASF']
-df11
+#Extraction des données relatives aux autoroutes A9,A709,A61,A62,A75 et A66
+data_route=df[(df.route=="A0009") | (df.route=="A0061") | (df.route=="A0062") | (df.route=="A0066") | (df.route=="A0075") | (df.route=="A0709")]
+data_route = data_route.reset_index()
+data_route
 
-# Nous allons extraire les villes qui nous interssent
+# %%
+# Transformation des coordonéées Lambert93 en coordonnées GPS
+from pyproj import Proj, transform
+
+X = data_route['x']
+Y = data_route['y']
+inProj = Proj(init='epsg:2154')
+outProj = Proj(init='epsg:4326')
+GPS = []
+
+for i in range (len(data_route)): 
+    GPS.append(transform(inProj,outProj,X[i],Y[i]))
 
 
 # %%
-df12 = df11()
+import requests
+import json
+dist=[]
+for i in range (len(GPS)):
+    if i-1 <0 :
+        x,y = GPS[i]
+    else : x,y = GPS[i-1]
+
+    x1,y1= GPS[i]
+
+    r = requests.get(f"http://router.project-osrm.org/route/v1/car/{x},{y};{x1},{y1}?overview=false""")
+    routes = json.loads(r.content)
+    route_1 = routes.get("routes")[0]
+    dist.append(round(route_1['distance']/1000))
+print(dist)
+
+
+# %%
