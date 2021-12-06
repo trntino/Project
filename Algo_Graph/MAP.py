9323d953b81076083019b24d9ab2a19936b9244c
ELKHMISSI <mohamed.el-khmissi01@etu.umontpellier.fr>
2021-12-06 15:50:29 +0100

    map

--- Algo_Graph/MAP.py
+++ Algo_Graph/MAP.py
@@ -7,7 +7,6 @@ import openrouteservice
 import json
 from ipywidgets import interact
 import os
-import Algorithme
 #%%
 price = pd.read_csv('prices_clean.csv')
 Coordinate = pd.read_csv('coordonnees_clean.csv')
@@ -16,93 +15,85 @@ Coordinate = pd.read_csv('coordonnees_clean.csv')
 Cities = sorted(Coordinate.NOMGARE.unique())
 
 #%%
-
-
 def road(DEPART, ARRIVEE):
-    i = Coordinate.loc[Coordinate['NOMGARE'] == DEPART].index[0]
-    j = Coordinate.loc[Coordinate['NOMGARE'] == ARRIVEE].index[0]
+    i = Coordinate[Coordinate['NOMGARE'] == DEPART].index[0]
+    j = Coordinate[Coordinate['NOMGARE'] == ARRIVEE].index[0]
 
     x = [Coordinate['X'][i],
          Coordinate['Y'][i]]
     y = [Coordinate['X'][j],
          Coordinate['Y'][j]]
+    m = folium.Map(
+                        location=[43.1837661, 3.0042121],
+                        zoom_start=10,
+                        control_scale=True)
+    client = openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
+
 
     #problem solving of different outward and return distances
     
     if i < j:
 
-        coor = [x, y]
+        cor = [x, y]
+        
 
-        client = openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
-
-        m = folium.Map(
-                        location=[43.1837661, 3.0042121],
-                        zoom_start=10,
-                        control_scale=True)
-
-        for i in range(0, len(coor)-1):
-            coord = coor[i], coor[i+1]
-            res = client.directions(coord)
+        for i in range(0, len(cor)-1):
+            crd = cor[i], cor[i+1]
+            crr = client.directions(crd)
 
             with(open('test.json', '+w')) as f:
-                f.write(json.dumps(res, indent=4, sort_keys=True))
+                f.write(json.dumps(crr, indent=4, sort_keys=True))
 
-                geometry = client.directions(coord)['routes'][0]['geometry']
+                geometry = client.directions(crd)['routes'][0]['geometry']
                 decoded = convert.decode_polyline(geometry)
 
-                distance_txt = "<h4> Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
+                distance_txt = "<h4> Distance :&nbsp" + "<strong>"+str(round(crr['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
                 price_txt = "<h4> Prix :&nbsp" + "<strong>" + str(price[DEPART][j]) + " € </strong>" + "</h4></b>"
 
                 folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+price_txt, max_width=300)).add_to(m)
 
                 folium.Marker(
-                            coord[0][::-1],
+                            crd[0][::-1],
                             popup=DEPART,
                             icon=folium.Icon(color='red', icon='car', prefix='fa')
                              ).add_to(m)
 
                 folium.Marker(
-                            coord[1][::-1],
+                            crd[1][::-1],
                             popup=ARRIVEE,
-                            icon=folium.Icon(color='red', icon='car', prefix='fa')
+                            icon=folium.Icon(color='blue', icon='car', prefix='fa')
                              ).add_to(m)
         return m
 
     elif i > j:
 
-        coor = [y, x]
+        cor = [y, x]
 
-        client = openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
-        m = folium.Map(
-                        location=[43.1837661, 3.0042121],
-                        zoom_start=10,
-                        control_scale=True)
-
-        for i in range(0, len(coor)-1):
-            coord = coor[i], coor[i+1]
-            res = client.directions(coord)
+        for i in range(0, len(cor)-1):
+            crd = cor[i], cor[i+1]
+            crr = client.directions(crd)
 
             with(open('test.json', '+w')) as f:
-                f.write(json.dumps(res, indent=4, sort_keys=True))
+                f.write(json.dumps(crr, indent=4, sort_keys=True))
 
-                geometry = client.directions(coord)['routes'][0]['geometry']
+                geometry = client.directions(crd)['routes'][0]['geometry']
                 decoded = convert.decode_polyline(geometry)
 
-                distance_txt = "<h4> Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
+                distance_txt = "<h4> Distance :&nbsp" + "<strong>"+str(round(crr['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
                 price_txt = "<h4> Prix :&nbsp" + "<strong>"+str(price[DEPART][j]) + " € </strong>" + "</h4></b>"
 
                 folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+price_txt, max_width=300)).add_to(m)
 
                 folium.Marker(
-                            coord[0][::-1],
+                            crd[0][::-1],
                             popup=DEPART,
                             icon=folium.Icon(color='red', icon='car', prefix='fa')
                              ).add_to(m)
 
                 folium.Marker(
-                            coord[1][::-1],
+                            crd[1][::-1],
                             popup=ARRIVEE,
-                            icon=folium.Icon(color='red', icon='car', prefix='fa')
+                            icon=folium.Icon(color='blue', icon='car', prefix='fa')
                              ).add_to(m)
         return m
 
@@ -112,5 +103,8 @@ def road(DEPART, ARRIVEE):
 
 
 interact(road, DEPART=Cities, ARRIVEE=Cities)
+
+
+# %%
 
 # %%
